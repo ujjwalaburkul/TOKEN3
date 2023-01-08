22811ae4678c9bd3851ca8b7e6082dd96b858afc
shrik <shrik@LAPTOP-5N61NU7D>
2023-01-08 17:01:35 +0530

    COMMIT

---
+++ src/demo1/sample1.java
@@ -0,0 +1,9 @@
+package demo1;
+
+public class sample1 
+{
+public static void main(String[] args) 
+{
+    System.out.println("Hello");
+}
+}
---
+++ src/demo1/sample2.java
@@ -0,0 +1,11 @@
+package demo1;
+
+public class sample2
+{
+	
+		public static void main(String[] args) 
+		{
+		    System.out.println("Hello Good NIGHT");
+		}
+		
+}
